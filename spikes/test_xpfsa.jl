71f1f0b874f6fcdb6b79f898b2925601f720adfb
Robert Feldt <robert.feldt@gmail.com>
2014-06-29 17:03:21 +0200

    more tests for empirical distr also when samples of different types

--- spikes/test_xpfsa.jl
+++ spikes/test_xpfsa.jl
@@ -7,6 +7,7 @@ distr = SymbolDistr({0 => 0.9, 1 => 0.1})
   @test in(s, [0, 1])
 end
 
+# Check that an empirical distribution can be created based on samples.
 ed1 = empirical_distribution([:a, :b])
 @test ed1[:a] == 0.5
 @test ed1[:b] == 0.5
@@ -14,6 +15,15 @@ ed1 = empirical_distribution([:a, :b])
 ed2 = empirical_distribution([:a, :b, :a, :a])
 @test ed2[:a] == 0.75
 @test ed2[:b] == 0.25
+
+ed3 = empirical_distribution([1, 0, 1, 1])
+@test ed3[1] == 0.75
+@test ed3[0] == 0.25
+
+ed4 = empirical_distribution([1, "2", 0, 1, 1, 0, "2", 1, 1, 0])
+@test ed4[0] == (3/10)
+@test ed4[1] == (5/10)
+@test ed4["2"] == (2/10)
 
 # Check that we get roughly the same distribution when sampling.
 N = 10000
