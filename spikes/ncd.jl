fa4eb363815d812044f96aea5ba4bc2f26b9711e
Robert Feldt <robert.feldt@gmail.com>
2014-11-10 00:01:44 +0100

    implemented the approx ncdl of Cohen and Vitanyi

--- spikes/ncd.jl
+++ spikes/ncd.jl
@@ -1,60 +1,85 @@
-using GZip
+using Zlib
 
-function tempfilename()
-  "tempfile_" * strftime("%Y%m%d_%H%M%S_", time()) * string(rand(1:1000000000)) * ".tmp"
+zliblength(str) = length(compress(str,9,false,true))
+
+# If we are using GZip we need to save to file:
+#using GZip
+#
+#function gzip_length(str) # We should not have to write to disk but couldn't find a way to do it in mem with GZip.jl. Investigate alternatives!
+#  fn = tempfilename()
+#  gfh = GZip.open(fn, "w")
+#  println(gfh, str)
+#  close(gfh)
+#  len = filesize(fn)
+#  rm(fn)
+#  len
+#end
+#function tempfilename()
+#  "tempfile_" * strftime("%Y%m%d_%H%M%S_", time()) * string(rand(1:1000000000)) * ".tmp"
+#end
+
+ncd(x, y, clen = zliblength) = ncd(string(x), string(y), clen)
+
+function ncd(x::ASCIIString, y::ASCIIString, clen = zliblength)
+  cxy = clen(x * y)
+  cx = clen(x)
+  cy = clen(y)
+  (cxy - min(cx, cy)) / max(cx, cy)
 end
 
-function gzip_length(str) # We should not have to write to disk but couldn't find a way to do it in mem with GZip.jl. Investigate alternatives!
-  fn = tempfilename()
-  gfh = GZip.open(fn, "w")
-  println(gfh, str)
-  close(gfh)
-  len = filesize(fn)
-  rm(fn)
-  len
+# NCD for lists is here called NCDL. It is an implementation of the "NCD for Multisets"
+# as described in the paper:
+#  A. Cohen & P. Vitanyi, "", 2012, http://arxiv.org/pdf/1212.5711.pdf
+# This is a heuristic to calc the NCDL in O(n^2) operations since the def of NCDL implies
+# a O(2^n) algorithm.
+function ncdl{T <: Any}(X::Array{T, 1}, clen = zliblength)
+  ncdl(map(string, X), clen)
 end
 
-function ncd(x, y, c = gzip_length)
-  xs = string(x)
-  ys = string(y)
+function ncdl(Xstrs::Array{ASCIIString, 1}, clen = zliblength, return_indexset = false)
+  Gxs = map(clen, Xstrs)
+  n = length(Xstrs)
 
-  cxy = c(xs * ys)
-  cx = c(xs)
-  cy = c(ys)
+  maxncd1 = -10.0 # Start with value < 0.0 to ensure first one is larger (NCD values are between 0.0 and 1+epsilon).
 
-  min_xy, max_xy = extrema([cx, cy])
+  # We will deselect one element in each step but start with all selected:
+  selected = collect(1:n) .> 0
+  selectedindices = Set(collect(1:n))
+  numselected = n
+  best = largest = 0
 
-  (cxy - min_xy) / max_xy
-end
+  while numselected >= 2
 
-# NCD for lists is here called NCDL
-function ncdl(X::Array{Any}, c = gzip_length)
-  Xstrs = map(string, X)
-  Xs = string(X)
+    # Calc max G(X \ {x}) for the still selected ones
+    maxGxminus1 = -Inf
+    for i in selectedindices
+      selected[i] = false
+      newGxminus1 = clen(join(Xstrs[selected], ""))
+      selected[i] = true
+      if newGxminus1 > maxGxminus1
+        maxGxminus1 = newGxminus1
+        largest = i
+      end
+    end
 
-  Gx = c(Xs)
-  Gxs = map(c, X)
-  EGmax = Gx - minimum(Gxs)
+    # NCD1 = (G(X) - minimum(G(X))) / maxGxminus1
+    ncd1 = (clen(join(Xstrs[selected], "")) - minimum(Gxs[selected])) / maxGxminus1
+    if ncd1 > maxncd1
+      maxncd1 = ncd1
+      best = copy(selectedindices)
+    end
+    selected[largest] = false
+    selectedindices = setdiff(selectedindices, Set(largest))
+    numselected -= 1
 
-  # Calculate the max Gx for each set minus one of the original elements
-  maxGxminus1 = -Inf
-  n = length(X)
-  for i in 1:n
-    Xminusi = X[[collect(1:(i-1)), collect((i+1):n)]]
-    maxGxminus1 = max(maxGxminus1, c(string(Xminusi)))
   end
 
-  # First term:
-  NCD1 = EGmax / maxGcminus1
-
-  # Second term is the max nlcd for any subset of X:
-  NCD2 = maximum(ncdl_proper_subsets(X)) # This will scale exponentially in the num of terms though...
-
-  # Now the ncdl is just the max of the 1st and 2nd terms:
-  max(NCD1, NCD2)
+  if return_indexset
+    return (maxncd1, best)
+  else
+    return maxncd1
+  end
 end
-
-# Implement the heuristic approximation of NCDL in the Cohen&Vitanyi 2012 paper instead.
 
 # We can then define the Test Set Diameter (TSD) as the NCDL of the test inputs or the test cases or the traces when executing these tests.
 # From TSD we can then define the Test Strategy Diameter for size N, TSD(N), as the average
@@ -67,3 +92,23 @@ end
 # with a statistically significantly higher TSD(N) than another strategy S2 (TSD(N)_S1 >~ TSD(N)_S2)
 # (evaluated with something like a Wilcoxon test, for example) is likely to yield higher fault 
 # finding ability.
+
+ncdl([1,2,3,4,5])
+
+# We expect a random uniform sampling to give a larger ncd than a normal sampled one if the latter
+# is confined to smaller range.
+N = 200
+ncdl(int(100.0 * rand(N)))
+ncdl(int(50 + 5*randn(N)))
+
+# We expect a generator of arrays of integers that can generate longer arrays to have larger NCDL
+# than one which generates smaller ints.
+genintarrays(maxsize, minInt = -100, maxInt = 100) = map((i) -> rand(minInt:maxInt), 1:rand(0:maxsize))
+ncdl(map((i) -> genintarrays(3), 1:N))
+ncdl(map((i) -> genintarrays(8), 1:N))
+
+# We expect generators generating arrays of same length to have larger ncd if they sample a 
+# larger span of the ints (on average):
+ncdl(map((i) -> genintarrays(5, -5, 5), 1:N))
+ncdl(map((i) -> genintarrays(5, -50, 50), 1:N))
+ncdl(map((i) -> genintarrays(5, -5000, 5000), 1:N))
