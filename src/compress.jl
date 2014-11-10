c8d280d011493ab7e050650cc4a16990fabd54e3
Robert Feldt <robert.feldt@gmail.com>
2014-11-10 13:52:57 +0100

    added notes on how to install Blosc

--- src/compress.jl
+++ src/compress.jl
@@ -6,6 +6,15 @@ zliblength(str) = length(Zlib.compress(str,9,false,true))
 # you should definetely use qlz4length, lz4length or lz4hclength
 # instead of zliblength as compressiors since they are typically
 # an order of magnitude faster.
+#
+# To compile Blosc I had to 
+#   1. brew install hdf5
+#   2. Pkg.clone("https://github.com/jakebolewski/Blosc.jl.git")
+#   3. cp ~/.julia/v0.3/Blosc/deps/build.jl ~/.julia/v0.3/Blosc/deps/deps.jl
+#   4. "using Blosc" at a Julia prompt
+#
+# See also: https://github.com/jakebolewski/Blosc.jl/issues/1
+#
 if isinstalled("Blosc")
   using Blosc
   lz4hclength(s) = length(Blosc.compress(convert(Vector{Uint8}, s), clevel=9, cname=:lz4hc))
